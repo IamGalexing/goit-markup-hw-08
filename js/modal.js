ac3a78e1fceb04ab7a62a37b83bd41203e5ced33
Oleksandr Gneushev <galexing@gmail.com>
2020-10-28 21:15:03 +0100

    version test

---
+++ js/modal.js
@@ -0,0 +1,27 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("backdrop--is-hidden");
+  }
+})();
+
+
+
+(() => {
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const mobileMenuRef = document.querySelector("[data-menu]");
+  
+
+  menuBtnRef.addEventListener("click", () => {
+    menuBtnRef.classList.toggle("is-open");
+    mobileMenuRef.classList.toggle("is-open");
+  });
+})();
